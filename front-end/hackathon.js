f36052c84f946a5e812ea29a53382b954540aa1d
JaredSchwartz1 <jaredschwartz04@gmail.com>
2024-03-28 07:21:49 -0400

    js

--- front-end/hackathon.js
+++ front-end/hackathon.js
@@ -1,1 +1,17 @@
 const hello="world"
+
+const fetchData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    console.log(res);
+    if (res.status === 200) {
+      const data = await res.json();
+      console.log(data);
+      data.forEach((element) => {
+        const h2 = document.createElement("h2");
+        h2.textContent = element.title;
+        body.append(h2);
+      });
+    } else {
+      console.log("there was an error ");
+    }
+  };
